4f4cb677544b65c510a16bdbee41606486c86cc2
SOHN DAI GEUN <nhos126@gmail.com>
2016-08-10 21:09:22 +0900

    Make cell swipeable

--- Goraebang/MyListTableViewController.swift
+++ Goraebang/MyListTableViewController.swift
@@ -25,13 +25,17 @@ class MyListTableViewController: UITableViewController {
         
         // table cell gesture recognizer 추가
         
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
+    
+    
+    
+    
     
     func getMyList(){
         let url:NSURL = NSURL(string: "http://52.78.101.90/json/song")!
@@ -39,40 +43,41 @@ class MyListTableViewController: UITableViewController {
         
         myListReadableJSON = JSON(data: jsonData, options: NSJSONReadingOptions.MutableContainers, error: nil)
         
+        
         print(myListReadableJSON[0]["lyrics"])
-//        print(myListReadableJSON.count)
-//        print(myListReadableJSON[0]["jacket"])
+        //        print(myListReadableJSON.count)
+        //        print(myListReadableJSON[0]["jacket"])
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         // 행의 개수는 JSON을 count한 것과 같다.
         return myListReadableJSON.count
     }
-
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("MyListTableCell", forIndexPath: indexPath) as! MyListTableViewCell
         
         let row = indexPath.row
-//        cell.songNumberLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+        //        cell.songNumberLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
         cell.songNumberLabel.font = cell.songNumberLabel.font.fontWithSize(12)
         cell.songNumberLabel.text = String(myListReadableJSON[row]["song_tjnum"])
-//        cell.songTitleLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+        //        cell.songTitleLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
         cell.songTitleLabel.font = cell.songTitleLabel.font.fontWithSize(12)
         cell.songTitleLabel.text = myListReadableJSON[row]["title"].string
-//        cell.songArtistLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+        //        cell.songArtistLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
         cell.songArtistLabel.font = cell.songArtistLabel.font.fontWithSize(12)
         cell.songArtistLabel.text = "Unkown Artist"
         
@@ -86,15 +91,15 @@ class MyListTableViewController: UITableViewController {
         // cell gesture recognizer
         
         
-
+        
         // Configure the cell...
-
+        
         return cell
     }
     
-//    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//        return "Section"
-//    }
+    //    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    //        return "Section"
+    //    }
     
     // MARK: Section Header
     override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
@@ -142,49 +147,74 @@ class MyListTableViewController: UITableViewController {
             detailViewController.lyrics = myListReadableJSON[row!]["lyrics"].string
             detailViewController.runtime = myListReadableJSON[row!]["runtime"].string
             detailViewController.song_tjnum = myListReadableJSON[row!]["song_tjnum"].string
-            var albumImageWebView:UIWebView = UIWebView()
+            let albumImageWebView:UIWebView = UIWebView()
             albumImageWebView.loadRequest(NSURLRequest(URL: NSURL(string: myListReadableJSON[row!]["jacket"].string!)!))
             detailViewController.albumWebView = albumImageWebView
             
         }
     }
     
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        if (editingStyle == UITableViewCellEditingStyle.Delete){
+            // json으로 삭제 요청하고
+            // mylistReadable json 다시 받은 뒤에 reload dAta
+            
+            self.tableView.reloadData()
+            
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITablseView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
     
-    
-
+    //    override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
+    //        var shareAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Share") { (action:UITableViewRowAction!, indexPath:NSIndexPath) in
+    //            print("hello")
+    //            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    //            // Write code here
+    //        }
+    //
+    //        return [shareAction]
+    //    }
 }
+
+/*
+ // Override to support conditional editing of the table view.
+ override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+ // Return false if you do not want the specified item to be editable.
+ return true
+ }
+ */
+
+/*
+ // Override to support editing the table view.
+ override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+ if editingStyle == .Delete {
+ // Delete the row from the data source
+ tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+ } else if editingStyle == .Insert {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }
+ }
+ */
+
+/*
+ // Override to support rearranging the table view.
+ override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+ 
+ }
+ */
+
+/*
+ // Override to support conditional rearranging of the table view.
+ override func tableView(tableView: UITablseView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+ // Return false if you do not want the item to be re-orderable.
+ return true
+ }
+ */
+
+
+
+
+
